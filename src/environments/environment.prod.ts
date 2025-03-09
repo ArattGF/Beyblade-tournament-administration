78f03b8f0f5178abd921b9057d4b8f1c63b67cdb
ArattGF <arattgf@gmail.com>
2025-03-08 21:52:28 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url.com/',
+  apiUrl: 'https://hidrobladers-api.vercel.app/',
   anotherVariable: 'someValue'
 };

f5e1cbf7bb5d074b1f33d27508694301f583c528
ArattGF <arattgf@gmail.com>
2025-03-08 21:48:17 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url.com/api',
+  apiUrl: 'https://your-production-api-url.com/',
   anotherVariable: 'someValue'
 };
